00ef24e3631826f147966fc6382e2c4bfbe1945b
Ethan-McGregor <ethanm4@uw.edu>
2017-01-16 10:51:04 -0800

    finished code & screenshots

--- app/src/main/java/activityspy/ethanm4/washington/edu/activityspy/MainActivity.java
+++ app/src/main/java/activityspy/ethanm4/washington/edu/activityspy/MainActivity.java
@@ -1,5 +1,9 @@
-package activityspy.ethanm4.washington.edu.activityspy;
+//Ethan McGregor
+//1-16-17
+//Activityspy
+//this program is intended to practice use of log for debugging purposes.
 
+package activityspy.ethanm4.washington.edu.activityspy;
 
     import android.support.v7.app.AppCompatActivity;
     import android.os.Bundle;
@@ -7,78 +11,56 @@ package activityspy.ethanm4.washington.edu.activityspy;
 
     public class MainActivity extends AppCompatActivity {
 
-        private static final String TAG = MainActivity.class.getName();
+        private static final String TAG = "MyActivity";
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_main);
-            Log.i(TAG, "onCreate event fired.");
+            Log.d(TAG, "onCreate event fired");
             if(savedInstanceState != null) {
-                Log.i(TAG, "onCreate event fired. " + savedInstanceState.toString());
+                Log.d(TAG, "onCreate event fired " + "(" + savedInstanceState.toString() + ")" );
+            }
+            if(1 + 1 == 3){
+                Log.wtf(TAG, "We have a problem....");
             }
         }
-
 
         @Override
         protected void onStart() {
             super.onStart();
-            Log.i(TAG, "onStart event fired.");
+            Log.d(TAG, "onStart event fired");
         }
-
 
         @Override
         protected void onResume() {
             super.onResume();
-            Log.i(TAG, "onResume event fired.");
+            Log.d(TAG, "onResume event fired");
         }
-
 
         @Override
         protected void onPause() {
             super.onPause();
-            Log.i(TAG, "onPause event fired.");
+            Log.d(TAG, "onPause event fired");
         }
-
-
 
         @Override
         protected void onRestart(){
             super.onRestart();
-            Log.i(TAG, "onRestart event fired.");
+            Log.d(TAG, "onRestart event fired");
         }
-
 
         @Override
         protected void onStop() {
             super.onStop();
-            Log.i(TAG, "onStop event fired.");
+            Log.d(TAG, "onStop event fired");
         }
 
         @Override
         protected void onDestroy() {
-            Log.i(TAG, "onDestroy event fired.");
-            Log.e(TAG, "We're going down, Captain!");
             super.onDestroy();
-
-
-
-
-
-
-
-
-
-
+            Log.d(TAG, "onDestroy event fired");
+            Log.w(TAG, "We're going down, Captain!");
 
         }
-
-
-
-
-
-
-
-
-
     }
